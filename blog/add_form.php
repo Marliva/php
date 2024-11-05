e51c0ea93bcdd8090d4826c1fef6c90c212a00c7
Cédric L <cedriclevasseur76@gmail.com>
2024-11-05 14:58:06 +0100

    Ajout d'un label pour le champ titre

--- blog/add_form.php
+++ blog/add_form.php
@@ -10,7 +10,8 @@
         <h1>Ajouter un article</h1>
         <form action="add_article.php" method="GET">
             <div>
-                <input type="text" name="title">
+            <label for="title">Titre de l'article</label>    
+            <input type="text" name="title" placeholder ="Entrez votre texte...">
             </div>
 
         </form>
