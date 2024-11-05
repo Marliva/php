264ef996f9d4eb3f29401755fccb47a2869a48d2
Cédric L <cedriclevasseur76@gmail.com>
2024-11-05 16:31:25 +0100

    Tri des article dans l'ordre décroissant

--- blog/add_article.php
+++ blog/add_article.php
@@ -1,3 +1,26 @@
 <?php
 
-echo 'Test';
+$title = $_POST['title'];
+$content = $_POST['content'];
+
+try {
+    $connexion = new PDO("mysql:host=localhost;dbname=blog_db", 'root', '');
+} catch (PDOException $error)
+{
+    echo "Une erreur s'est produite lors de la connexion à votre base de données : " . $error->getMessage();
+}
+
+$connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$stm = $connexion->prepare("INSERT INTO article (title, content) VALUES (:title,:content)");
+
+$stm->bindParam(':title', $title);
+$stm->bindParam(':content', $content);
+
+if ($stm->execute()):
+    header("Location: index.php");
+else:
+    echo "ERREUR !!!";
+endif;
+
+$connexion = null;
