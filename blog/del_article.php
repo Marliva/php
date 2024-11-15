4d9ab95e68604ecbaaf8912f64442ce34318d11b
Cédric L <cedriclevasseur76@gmail.com>
2024-11-15 14:14:53 +0100

    Ajout fonction suppression article

--- blog/del_article.php
+++ blog/del_article.php
@@ -1,3 +1,17 @@
 <?php
 
-echo "Delete";
+try {
+
+    $id = $_GET['id'];
+
+    $connexion = new PDO("mysql:host=localhost;dbname=blog_db", 'root', '');
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $connexion->prepare('DELETE FROM article WHERE id = :id');
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+
+    header('hx-refresh: true');
+
+} catch (PDOException $error) {
+    echo "Erreur de connnexion à la base de données : " . $error->getMessage();
+}
